f0beb86a9326938383c6cb60cfb249acf8dcc833
atefeheydari <157300419+atefehy@users.noreply.github.com>
2024-02-23 14:02:08 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hi atefe");
+
+  let person={
+      fristname:'ati',
+      lastname:'hy'
+  };
+  person.fristname='me';
+  console.log(person);
+  let colors=['red','blue'];
+  colors[5]='purple';
+  console.log(colors);
+  function hi(name){
+    console.log("hi "+ name)
+  }
+name=hi("bibi");
